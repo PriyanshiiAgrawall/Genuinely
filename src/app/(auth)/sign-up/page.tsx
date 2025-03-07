421766367d65ae7cead8d2f6ffc195824013868a
Priyanshi Agrawal <priyanshiprofessional1@gmail.com>
2025-03-08 03:38:59 +0530

    Added logo navbar to signup and signin pages

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -28,6 +28,7 @@ import { Loader2 } from "lucide-react";
 import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import Navbar0 from "@/app/[uniqueName]/[spaceName]/components/Navbar0";
 
 export const signUpSchemaZod = z.object({
     name: uniqueNameZod,
@@ -148,118 +149,119 @@ export default function SignUpPage() {
     }
 
     return (
+        <div>
+            <Navbar0 />
+            <div className="flex justify-center items-center min-h-screen bg-[#000421] py-12 overflow-hidden">
+                <div className="w-full max-w-xl lg:w-1/2 xl:w-3/5 p-10 space-y-8 bg-[#F9FAFB] rounded-2xl shadow-2xl overflow-y-auto">
+                    <div className="text-center">
+                        <h1 className="text-4xl font-extrabold tracking-tight text-richblack-900 lg:text-5xl mb-4">
+                            Genuinely{` :)`} Genuine Testimonial Universe
+                        </h1>
+                        <p className="text-lg text-richblack-700 mb-6">Sign up to get your wall of lovely testimonials.</p>
+                    </div>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                            <FormField
+                                name="name"
+                                control={form.control}
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="text-richblack-900">Unique Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Unique name" {...field}
+                                                className="border-richblack-300 focus:border-[#272E3F] focus:ring-[#272E3F]"
+                                                onChange={(e: any) => {
+                                                    field.onChange(e)
+                                                    debounced(e.target.value)
+                                                }}
+                                            />
+                                        </FormControl>
+                                        <FormDescription className="text-sm text-blue-500">
+                                            {uniqueNameCheckingLoader && <Loader2 className="animate-spin text-[#000421]" />}
+                                            {uniqueName && <span className={`text-sm ${uniqueNameMessage === "This unique name is available" ? 'text-[#000421]' : 'text-red-500'}`}>{uniqueNameMessage}</span>}
 
-        <div className="flex justify-center items-center min-h-screen bg-[#000421] py-12 overflow-hidden">
-            <div className="w-full max-w-xl lg:w-1/2 xl:w-3/5 p-10 space-y-8 bg-[#F9FAFB] rounded-2xl shadow-2xl overflow-y-auto">
-                <div className="text-center">
-                    <h1 className="text-4xl font-extrabold tracking-tight text-richblack-900 lg:text-5xl mb-4">
-                        Genuinely{` :)`} Genuine Testimonial Universe
-                    </h1>
-                    <p className="text-lg text-richblack-700 mb-6">Sign up to get your wall of lovely testimonials.</p>
-                </div>
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-                        <FormField
-                            name="name"
-                            control={form.control}
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-richblack-900">Unique Name</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Unique name" {...field}
-                                            className="border-richblack-300 focus:border-[#272E3F] focus:ring-[#272E3F]"
-                                            onChange={(e: any) => {
-                                                field.onChange(e)
-                                                debounced(e.target.value)
-                                            }}
-                                        />
-                                    </FormControl>
-                                    <FormDescription className="text-sm text-blue-500">
-                                        {uniqueNameCheckingLoader && <Loader2 className="animate-spin text-[#000421]" />}
-                                        {uniqueName && <span className={`text-sm ${uniqueNameMessage === "This unique name is available" ? 'text-[#000421]' : 'text-red-500'}`}>{uniqueNameMessage}</span>}
-
-                                    </FormDescription>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            name="email"
-                            control={form.control}
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-richblack-900">Email</FormLabel>
-                                    <FormControl>
-                                        <Input placeholder="Email" {...field}
-                                            className="border-richblack-300 focus:border-[#272E3F] focus:ring-[#272E3F]"
-                                        />
-                                    </FormControl>
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            name="password"
-                            control={form.control}
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="text-richblack-900">Password</FormLabel>
-                                    <FormControl>
-                                        <Input type="password" placeholder="Password" {...field}
-                                            className="border-richblack-300 focus:border-[#272E3F] focus:ring-[#272E3F]"
-                                        />
-                                    </FormControl>
-                                </FormItem>
-                            )}
-                        />
-                        <Button type="submit" disabled={isSubmitting} className="w-full bg-[#272E3F] hover:bg-[#1e2433] text-white rounded-lg py-3">
-                            {isSubmitting ? (<><Loader2 className="mr-2 h-5 w-5 animate-spin text-[#000421]" /> Please Wait...</>) : ('Signup With Credentials')}
+                                        </FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                name="email"
+                                control={form.control}
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="text-richblack-900">Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Email" {...field}
+                                                className="border-richblack-300 focus:border-[#272E3F] focus:ring-[#272E3F]"
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                name="password"
+                                control={form.control}
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="text-richblack-900">Password</FormLabel>
+                                        <FormControl>
+                                            <Input type="password" placeholder="Password" {...field}
+                                                className="border-richblack-300 focus:border-[#272E3F] focus:ring-[#272E3F]"
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit" disabled={isSubmitting} className="w-full bg-[#272E3F] hover:bg-[#1e2433] text-white rounded-lg py-3">
+                                {isSubmitting ? (<><Loader2 className="mr-2 h-5 w-5 animate-spin text-[#000421]" /> Please Wait...</>) : ('Signup With Credentials')}
+                            </Button>
+                        </form>
+                    </Form>
+                    <hr></hr>
+                    <div className="space-y-4">
+                        <Button disabled={isSubmitting} onClick={googleSignUpHandler}
+                            className="w-full bg-[#272E3F] hover:bg-[#1e2433] text-white rounded-lg py-3">
+                            {
+                                <>
+                                    <FcGoogle className="mr-2 h-5 w-5" /> Signup With Google
+                                </>
+                            }
                         </Button>
-                    </form>
-                </Form>
-                <hr></hr>
-                <div className="space-y-4">
-                    <Button disabled={isSubmitting} onClick={googleSignUpHandler}
-                        className="w-full bg-[#272E3F] hover:bg-[#1e2433] text-white rounded-lg py-3">
-                        {
-                            <>
-                                <FcGoogle className="mr-2 h-5 w-5" /> Signup With Google
-                            </>
-                        }
-                    </Button>
-                    <Button disabled={isSubmitting} onClick={githubSignUpHandler}
-                        className="w-full bg-[#272E3F] hover:bg-[#1e2433] text-white rounded-lg py-3">
-                        {
-                            <>
-                                <FaGithub className="mr-2 h-5 w-5" /> Signup With Github
-                            </>
-                        }
-                    </Button>
-                    <Button
-                        disabled={isSubmitting}
-                        onClick={async (e) => {
-                            e.preventDefault();
-                            await signIn("credentials", {
-                                email: "priyanshi11",
-                                password: "12345678",
-                                redirect: true,
-                                callbackUrl: "/dashboard",
-                            });
-                        }}
+                        <Button disabled={isSubmitting} onClick={githubSignUpHandler}
+                            className="w-full bg-[#272E3F] hover:bg-[#1e2433] text-white rounded-lg py-3">
+                            {
+                                <>
+                                    <FaGithub className="mr-2 h-5 w-5" /> Signup With Github
+                                </>
+                            }
+                        </Button>
+                        <Button
+                            disabled={isSubmitting}
+                            onClick={async (e) => {
+                                e.preventDefault();
+                                await signIn("credentials", {
+                                    email: "priyanshi11",
+                                    password: "12345678",
+                                    redirect: true,
+                                    callbackUrl: "/dashboard",
+                                });
+                            }}
 
-                        className="w-full bg-[#272E3F] hover:bg-[#1e2433] text-white rounded-lg py-3"
-                    >
-                        Sign In With Dummy Data
-                    </Button>
+                            className="w-full bg-[#272E3F] hover:bg-[#1e2433] text-white rounded-lg py-3"
+                        >
+                            Sign In With Dummy Data
+                        </Button>
+                    </div>
+                    <div className="text-center mt-6">
+                        <p className="text-richblack-600">
+                            Already a member?{' '}
+                            <Link href="/sign-in" className="text-blue-600 hover:text-blue-800 font-semibold">
+                                Sign in
+                            </Link>
+                        </p>
+                    </div>
                 </div>
-                <div className="text-center mt-6">
-                    <p className="text-richblack-600">
-                        Already a member?{' '}
-                        <Link href="/sign-in" className="text-blue-600 hover:text-blue-800 font-semibold">
-                            Sign in
-                        </Link>
-                    </p>
-                </div>
-            </div>
-        </div>
+            </div>  </div>
     );
 }
