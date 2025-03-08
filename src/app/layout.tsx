74ba291f139083972c93f2e4ffd62aabe332e58f
Priyanshi Agrawal <priyanshiprofessional1@gmail.com>
2025-03-08 19:35:30 +0530

    Fixed name in browser tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Genuinely",
+  title: "Genuinely:Genuine Testimonial",
   description: "Genuine Testimonial Collection",
 };
 
