f9c262f919e42c92b6cad3ce29a6c8529636eac4
Priyanshi Agrawal <priyanshiprofessional1@gmail.com>
2025-03-08 21:19:02 +0530

    Fixed linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,10 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"), {
     rules: {
-      "no-unused-vars": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+
     },
   },
 ];
